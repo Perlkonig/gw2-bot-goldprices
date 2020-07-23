704ba85290bf98b4d70be7dd9ee7ee8f29e87107
Aaron Dalton <aaron@daltons.ca>
2020-07-23 11:20:18 -0600

    Broke out the commands

---
+++ commands/stats.js
@@ -0,0 +1,164 @@
+const Discord = require('discord.js');
+const { CanvasRenderService } = require('chartjs-node-canvas');
+const mergeImages = require('merge-images');
+const { Canvas, Image } = require('canvas');
+
+const width = 800; //px
+const height = 400; //px
+
+const chartJsFactory = () => {
+    const chartJS = require('chart.js');
+    require('chartjs-chart-box-and-violin-plot');
+    delete require.cache[require.resolve('chart.js')];
+    delete require.cache[require.resolve('chartjs-chart-box-and-violin-plot')];
+    return chartJS;
+};
+
+const chartCallback = (ChartJS) => {
+
+    // Global config example: https://www.chartjs.org/docs/latest/configuration/
+    // ChartJS.defaults.global.elements.rectangle.borderWidth = 2;
+    // // Global plugin example: https://www.chartjs.org/docs/latest/developers/plugins.html
+	ChartJS.plugins.register({
+        beforeDraw: (chart, options) => {
+            const ctx = chart.ctx;
+            ctx.save();
+            ctx.fillStyle = '#fff';
+            ctx.fillRect(0, 0, width, height);
+            ctx.restore();
+        }
+    });    
+    // ChartJS.plugins.register({
+    //     // plugin implementation
+    // });
+    // // New chart type example: https://www.chartjs.org/docs/latest/developers/charts.html
+    // ChartJS.controllers.MyType = ChartJS.DatasetController.extend({
+    //     // chart implementation
+    // });
+};
+const canvasRenderService = new CanvasRenderService(width, height, chartCallback, undefined, chartJsFactory);
+
+module.exports = {
+	name: 'stats',
+    description: 'Displays gold price statistics and graphs',
+    args: false,
+    aliases: ['goldstats'],
+	execute(message, args, stats) {
+        // Generate graph
+        (async () => {
+            const configuration = {
+                type: 'horizontalBoxplot',
+                data: {
+                    datasets: [{
+                        label: "Price History",
+                        data: [stats.allpoints],
+                        backgroundColor: '#f9a60240',
+                        borderColor: '#f9a602',
+                        borderWidth: 1,
+                        itemStyle: 'circle',
+                        itemRadius: 3,
+                        itemBackgroundColor: '#0000'
+                    }]
+                },
+                options: {
+                    legend: {
+                        display: false
+                    },
+                    title: {
+                        display: false,
+                        text: 'Price History'
+                    }
+                }
+            };            
+            return await canvasRenderService.renderToBuffer(configuration);
+            // return await canvasRenderService.renderToDataURL(configuration);
+            // return canvasRenderService.renderToStream(configuration);
+    
+        })()
+        .then((box) => {
+            (async () => {
+                const configuration = {
+                    type: 'line',
+                    data: {
+                        labels: stats.labels,
+                        datasets: [{
+                            label: 'Gems per 250 gold',
+                            data: stats.points,
+                            backgroundColor: '#f9a602',
+                            borderColor: '#f9a602',
+                            fill: false,
+                            borderWidth: 1
+                            }]
+                    },
+                    options: {
+                        title: {
+                            display: true,
+                            text: 'Price History'
+                        },
+                        scales: {
+                            xAxes: [{
+                                display: true,
+                                scaleLabel: {
+                                    display: true,
+                                    labelString: 'Date'
+                                }
+                            }],
+                            yAxes: [{
+                                display: true,
+                                scaleLabel: {
+                                    display: true,
+                                    labelString: 'Gems per 250 gold'
+                                }
+                            }]
+                        }
+                    }
+                };            
+                return [box, await canvasRenderService.renderToBuffer(configuration)];
+                // return await canvasRenderService.renderToDataURL(configuration);
+                // return canvasRenderService.renderToStream(configuration);
+            })()
+            .then((figs) => {
+                return mergeImages([
+                    {src: figs[1], x: 0, y: 0},
+                    {src: figs[0], x: 0, y:401}
+                ], {Canvas: Canvas,Image: Image, height: 800});
+            })
+            .then((b64) => {
+                var matches = b64.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+                response = {};
+
+                if (matches.length !== 3) {
+                    return new Error('Invalid input string');
+                }
+
+                response.type = matches[1];
+                response.data = new Buffer.from(matches[2], 'base64');
+
+                return response.data;
+            })
+            .then((img) => {
+                // Generate embed
+                const attachment = new Discord.MessageAttachment(img, 'graphs.png');
+    
+                const embed = new Discord.MessageEmbed()
+                .setColor('#f9a602')
+                .setTitle('GW2 Gold Price Notifier')
+                .setURL('https://github.com/Perlkonig/gw2-bot-goldprices')
+                // .setAuthor('Aaron Dalton', undefined, 'https://www.perlkonig.com')
+                .setDescription('Statistical Report')
+                // .setThumbnail('https://i.imgur.com/wSTFkRM.png')
+                .addField('Checking Frequency', stats.interval/1000 + " seconds", true)
+                .addField('Notification Threshold', stats.threshold, true)
+                .addField(stats.numpoints + " datapoints over " + stats.numdays + " days", "\u200B")
+                .addField('Price at Last Check', stats.lastprice, true)
+                .addField('Current Seven-Day Low', stats.minprice, true)
+                .attachFiles(attachment)
+                .setImage("attachment://graphs.png")
+                .setTimestamp()
+                .setFooter('All prices are /gems per 250 gold/');
+    
+                message.channel.send(embed);
+            });
+        })        
+	},
+};
--- index.js
+++ index.js
@@ -1,53 +1,23 @@
+const fs = require('fs');
 const Discord = require('discord.js');
-const auth = require('./auth.json');
+const config = require('./config.json');
 const fetch = require('node-fetch');
 const sqlite3 = require('sqlite3').verbose();
-const { CanvasRenderService } = require('chartjs-node-canvas');
-const mergeImages = require('merge-images');
-const { Canvas, Image } = require('canvas');
 
 // Initialize Discord Bot
 var bot = new Discord.Client();
+bot.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	bot.commands.set(command.name, command);
+}
 
 // Establish global vars
 var db, checker;
-const interval = 600000; //600000; //10 minutes
+const interval = 600000; //10 minutes
 const threshold = 925;
-const stats = {};
-const width = 800; //px
-const height = 400; //px
-
-const chartJsFactory = () => {
-    const chartJS = require('chart.js');
-    require('chartjs-chart-box-and-violin-plot');
-    delete require.cache[require.resolve('chart.js')];
-    delete require.cache[require.resolve('chartjs-chart-box-and-violin-plot')];
-    return chartJS;
-};
-
-const chartCallback = (ChartJS) => {
-
-    // Global config example: https://www.chartjs.org/docs/latest/configuration/
-    // ChartJS.defaults.global.elements.rectangle.borderWidth = 2;
-    // // Global plugin example: https://www.chartjs.org/docs/latest/developers/plugins.html
-	ChartJS.plugins.register({
-        beforeDraw: (chart, options) => {
-            const ctx = chart.ctx;
-            ctx.save();
-            ctx.fillStyle = '#fff';
-            ctx.fillRect(0, 0, width, height);
-            ctx.restore();
-        }
-    });    
-    // ChartJS.plugins.register({
-    //     // plugin implementation
-    // });
-    // // New chart type example: https://www.chartjs.org/docs/latest/developers/charts.html
-    // ChartJS.controllers.MyType = ChartJS.DatasetController.extend({
-    //     // chart implementation
-    // });
-};
-const canvasRenderService = new CanvasRenderService(width, height, chartCallback, undefined, chartJsFactory);
+const stats = {interval: interval, threshold: threshold};
 
 bot.once('ready', function (evt) {
     console.log('Connected');
@@ -61,133 +31,25 @@ bot.once('ready', function (evt) {
     checker = bot.setInterval(checkPrice, interval);
 });
 
-const prefix = '!'
-
 bot.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+	if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/);
+	const commandName = args.shift().toLowerCase();
 
-	if (command === 'ping') {
-		message.channel.send('Pong.');
-	} else if (command === 'stats') {
-        // Generate graph
-        (async () => {
-            const configuration = {
-                type: 'horizontalBoxplot',
-                data: {
-                    datasets: [{
-                        label: "Price History",
-                        data: [stats.allpoints],
-                        backgroundColor: '#f9a60240',
-                        borderColor: '#f9a602',
-                        borderWidth: 1,
-                        itemStyle: 'circle',
-                        itemRadius: 3,
-                        itemBackgroundColor: '#0000'
-                    }]
-                },
-                options: {
-                    legend: {
-                        display: false
-                    },
-                    title: {
-                        display: false,
-                        text: 'Price History'
-                    }
-                }
-            };            
-            return await canvasRenderService.renderToBuffer(configuration);
-            // return await canvasRenderService.renderToDataURL(configuration);
-            // return canvasRenderService.renderToStream(configuration);
-    
-        })()
-        .then((box) => {
-            (async () => {
-                const configuration = {
-                    type: 'line',
-                    data: {
-                        labels: stats.labels,
-                        datasets: [{
-                            label: 'Gems per 250 gold',
-                            data: stats.points,
-                            backgroundColor: '#f9a602',
-                            borderColor: '#f9a602',
-                            fill: false,
-                            borderWidth: 1
-                            }]
-                    },
-                    options: {
-                        title: {
-                            display: true,
-                            text: 'Price History'
-                        },
-                        scales: {
-                            xAxes: [{
-                                display: true,
-                                scaleLabel: {
-                                    display: true,
-                                    labelString: 'Date'
-                                }
-                            }],
-                            yAxes: [{
-                                display: true,
-                                scaleLabel: {
-                                    display: true,
-                                    labelString: 'Gems per 250 gold'
-                                }
-                            }]
-                        }
-                    }
-                };            
-                return [box, await canvasRenderService.renderToBuffer(configuration)];
-                // return await canvasRenderService.renderToDataURL(configuration);
-                // return canvasRenderService.renderToStream(configuration);
-            })()
-            .then((figs) => {
-                return mergeImages([
-                    {src: figs[1], x: 0, y: 0},
-                    {src: figs[0], x: 0, y:401}
-                ], {Canvas: Canvas,Image: Image, height: 800});
-            })
-            .then((b64) => {
-                var matches = b64.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
-                response = {};
+    const command = bot.commands.get(commandName)
+        || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    if (!command) return;    
 
-                if (matches.length !== 3) {
-                    return new Error('Invalid input string');
-                }
+    if (command.args && !args.length) {
+        return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+    }
 
-                response.type = matches[1];
-                response.data = new Buffer.from(matches[2], 'base64');
-
-                return response.data;
-            })
-            .then((img) => {
-                // Generate embed
-                const attachment = new Discord.MessageAttachment(img, 'graphs.png');
-    
-                const embed = new Discord.MessageEmbed()
-                .setColor('#f9a602')
-                .setTitle('GW2 Gold Price Notifier')
-                .setURL('https://github.com/Perlkonig/gw2-bot-goldprices')
-                // .setAuthor('Aaron Dalton', undefined, 'https://www.perlkonig.com')
-                .setDescription('Statistical Report')
-                // .setThumbnail('https://i.imgur.com/wSTFkRM.png')
-                .addField('Checking Frequency', interval/1000 + " seconds", true)
-                .addField('Notification Threshold', threshold, true)
-                .addField(stats.numpoints + " datapoints over " + stats.numdays + " days", "\u200B")
-                .addField('Price at Last Check', stats.lastprice, true)
-                .addField('Current Seven-Day Low', stats.minprice, true)
-                .attachFiles(attachment)
-                .setImage("attachment://graphs.png")
-                .setTimestamp()
-                .setFooter('All prices are /gems per 250 gold/');
-    
-                message.channel.send(embed);
-            });
-        })
+    try {
+        command.execute(message, args, stats);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
     }
 })
 
@@ -307,4 +169,4 @@ function storePrice(price) {
     });
 }
 
-bot.login(auth.token);
+bot.login(config.token);
